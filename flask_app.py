47c80f395725fc9d749db33bd438e97314ab7198
EeOneDown <rentgen.stalker@yandex.ru>
2017-11-08 22:21:19 +0300

    changed bot name

--- flask_app.py
+++ flask_app.py
@@ -479,8 +479,9 @@ def return_hided_lesson(message):
                      reply_markup=ids_keyboard, parse_mode="HTML")
 
 
+# TODO change bot name
 @bot.message_handler(func=lambda mess: mess.reply_to_message is not None and
-                     mess.reply_to_message.from_user.username == "Spbu4UBot" and
+                     mess.reply_to_message.from_user.username == "tt202_bot" and
                      mess.reply_to_message.text == "Напиши мне что-нибудь:",
                      content_types=["text"])
 def users_callback_handler(message):
@@ -542,8 +543,9 @@ def educator_schedule_handler(message):
                      parse_mode="HTML")
 
 
+# TODO change bot name
 @bot.message_handler(func=lambda mess: mess.reply_to_message is not None and
-                     mess.reply_to_message.from_user.username == "Spbu4UBot" and
+                     mess.reply_to_message.from_user.username == "tt202_bot" and
                      "Введи Фамилию преподавателя:" in
                      mess.reply_to_message.text,
                      content_types=["text"])

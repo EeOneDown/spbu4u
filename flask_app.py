5ab75ff8dff48b9d52215c295b9640932f9e6a89
EeOneDown <rentgen.stalker@yandex.ru>
2017-11-22 17:42:32 +0300

    refactor schedule_editor

--- flask_app.py
+++ flask_app.py
@@ -1231,7 +1231,7 @@ def select_day_handler(call_back):
     events_keyboard = telebot.types.InlineKeyboardMarkup(True)
     events = first_block.split("\n\n")[1:-1]
     for num, event in enumerate(events, start=1):
-        event_name = event[3:-4].split(" - ")
+        event_name = event.split(" - ")
         button_text = "{0}. {1} - {2}".format(num, event_name[0],
                                               event_name[1].split(". ")[-1])
         events_keyboard.row(

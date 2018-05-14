c3ef96151781cea5f3508c59cff8d4115ebf81a9
eeonedown <rentgen.stalker@yandex.ru>
2018-05-14 14:51:42 +0300

    moved from bot/

--- schedule_sender.py
+++ schedule_sender.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from datetime import datetime, timedelta
 
 from bot import bot
+from bot.constants import server_timedelta
 from bot.functions import get_json_day_data, create_schedule_answer, \
     is_full_place, send_long_message, get_connection
 
@@ -18,23 +19,28 @@ def schedule_sender():
     cursor.close()
     sql_con.close()
 
-    tomorrow_moscow_datetime = datetime.today() + timedelta(days=1, hours=3)
+    tomorrow_moscow_datetime = \
+        datetime.today() + timedelta(days=1) + server_timedelta
     tomorrow_moscow_date = tomorrow_moscow_datetime.date()
+
     for user_data in data:
         user_id = user_data[0]
         json_day = get_json_day_data(user_id, tomorrow_moscow_date)
         full_place = is_full_place(user_id)
         answer = create_schedule_answer(json_day, full_place, user_id)
+
         if "Выходной" in answer:
             continue
-        print(user_id, answer)
+
         try:
             answer = "Расписание на завтра:\n\n" + answer
             send_long_message(bot, answer, user_id)
         except Exception as err:
             print(err)
+            print(user_id)
             continue
 
 
 if __name__ == '__main__':
     schedule_sender()
+    print("Done", datetime.today().time())

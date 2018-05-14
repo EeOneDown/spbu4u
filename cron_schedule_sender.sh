3febeb4adbcef4b2a17bb79fe7b1740c50cd5d76
eeonedown <rentgen.stalker@yandex.ru>
2018-05-14 16:34:33 +0300

    bug fixes

--- cron_schedule_sender.sh
+++ cron_schedule_sender.sh
@@ -4,7 +4,6 @@ dir_path=$1
 
 if ! [ -d "$dir_path/logs/" ]; then
     mkdir "$dir_path/logs"
-    ls
 fi
 
 ${dir_path}/venv/bin/python ${dir_path}/schedule_sender.py 1>>${dir_path}/logs/schedule_sender_log.txt 2>>${dir_path}/logs/schedule_sender_err.txt

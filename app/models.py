fd2e9e66ece50a4ab7e785ada7e504a57c38770a
eeonedown <rentgen.stalker@yandex.ru>
2018-06-26 20:11:07 +0300

    db models creating

--- app/models.py
+++ app/models.py
@@ -1,6 +1,42 @@
 from app import db
 
 
+users_groups = db.Table(
+    "users_groups",
+    db.Column("group_id", db.Integer, db.ForeignKey("groups.id"),
+              primary_key=True),
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id"),
+              primary_key=True)
+)
+
+
+users_added_lesson = db.Table(
+    "users_added_lesson",
+    db.Column("lesson", db.Integer, db.ForeignKey("lessons.id"),
+              primary_key=True),
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id"),
+              primary_key=True)
+)
+
+
+users_hidden_lessons = db.Table(
+    "users_hidden_lessons",
+    db.Column("lesson", db.Integer, db.ForeignKey("lessons.id"),
+              primary_key=True),
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id"),
+              primary_key=True)
+)
+
+
+users_chosen_educators = db.Table(
+    "users_chosen_educators",
+    db.Column("lesson", db.Integer, db.ForeignKey("lessons.id"),
+              primary_key=True),
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id"),
+              primary_key=True)
+)
+
+
 class User(db.Model):
     __tablename__ = "users"
 
@@ -13,20 +49,34 @@ class User(db.Model):
     univer_station_code = db.Column(db.String(10), default="s9603770",
                                     nullable=False)
     current_group_id = db.Column(db.Integer, db.ForeignKey("groups.id"))
-    current_group = db.relationship("Group")
-
-    # captains_teams = db.relationship('Team', backref='captain', lazy='dynamic')
+    groups = db.relationship("Group", secondary=users_groups,
+                             back_populates="members", lazy="dynamic")
+    added_lessons = db.relationship("Lesson", secondary=users_added_lesson,
+                                    lazy="dynamic")
+    hidden_lessons = db.relationship("Lesson", secondary=users_hidden_lessons,
+                                     lazy="dynamic")
+    chosen_educators = db.relationship("Lesson",
+                                       secondary=users_chosen_educators,
+                                       lazy="dynamic")
 
 
 class Group(db.Model):
     __tablename__ = "groups"
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
+    title = db.Column(db.String(128))
+    members = db.relationship("User", secondary=users_groups,
+                              back_populates="groups", lazy="dynamic")
 
 
 class Lesson(db.Model):
     __tablename__ = "lessons"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-
+    name = db.Column(db.String(128))
+    types = db.Column(db.String)        # mb JSON/ARRAY?
+    days = db.Column(db.String)         # mb JSON/ARRAY?
+    times = db.Column(db.String)        # mb JSON/ARRAY?
+    educators = db.Column(db.String)    # mb JSON/ARRAY?
+    __table_args__ = (db.UniqueConstraint("name", "types", "days", "times",
+                                          "educators"), )

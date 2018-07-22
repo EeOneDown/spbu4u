fb797472925877da3e33617b8344ff21bbe2313e
eeonedown <rentgen.stalker@yandex.ru>
2018-07-23 02:25:40 +0300

    closes #49

--- tg_bot/handlers/suburbans/personalization.py
+++ tg_bot/handlers/suburbans/personalization.py
@@ -3,32 +3,35 @@ from __future__ import unicode_literals
 
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-from tg_bot import bot, functions as func
-from app.constants import emoji, all_stations
+from tg_bot import bot
+from app.constants import (
+    emoji, all_stations, personalization_answer, select_home_station,
+    select_univer_station
+)
+from app import new_functions as nf, db
+from flask import g
+import telebot_login
 
 
 # Personalization message
 @bot.message_handler(func=lambda mess: mess.text.title() == "Персонализация",
                      content_types=["text"])
+@telebot_login.login_required
 def personalisation_handler(message):
-    home_station = func.get_station_code(message.chat.id, is_home=True)
-    univer_station = func.get_station_code(message.chat.id, is_home=False)
+    user = g.current_tbot_user
 
-    home_station_title = func.get_key_by_value(all_stations, home_station)
-    univer_station_title = func.get_key_by_value(all_stations, univer_station)
+    home_title = nf.get_key_by_value(all_stations, user.home_station_code)
+    univer_title = nf.get_key_by_value(all_stations, user.univer_station_code)
 
-    answer = "Здесь ты можешь настроить <b>домашнюю</b> и " \
-             "<b>Университетскую</b> станции для команд <i>Домой</i> и " \
-             "<i>В Универ</i>\n\n" \
-             "<b>Домашняя:</b> {0}\n<b>Университетская:</b> {1}".format(
-                                    home_station_title, univer_station_title)
+    answer = personalization_answer.format(home_title, univer_title)
 
-    inline_keyboard = InlineKeyboardMarkup(True)
+    inline_keyboard = InlineKeyboardMarkup()
     inline_keyboard.row(*[InlineKeyboardButton(
             text=name, callback_data=name) for name in ["Домашняя"]])
     inline_keyboard.row(*[InlineKeyboardButton(
             text=name, callback_data=name) for name in ["Университетская"]])
-    bot.send_message(message.chat.id, answer,
+
+    bot.send_message(user.tg_id, answer,
                      reply_markup=inline_keyboard, parse_mode="HTML")
 
 
@@ -37,51 +40,65 @@ def personalisation_handler(message):
                             call_back.data == "Домашняя")
 @bot.callback_query_handler(func=lambda call_back:
                             call_back.data == "Университетская")
+@telebot_login.login_required_callback
 def home_station_handler(call_back):
+    user = g.current_tbot_user
+
     if call_back.data == "Домашняя":
-        type_station = "домашнюю"
+        answer = select_home_station
     else:
-        type_station = "университетскую"
-    answer = "Выбери {} станцию:".format(type_station)
-    stations_keyboard = InlineKeyboardMarkup(True)
-    for item in all_stations.items():
-        stations_keyboard.row(*[InlineKeyboardButton(
-            text=item[0], callback_data=all_stations[item[0]])])
+        answer = select_univer_station
+
+    stations_keyboard = InlineKeyboardMarkup()
+    stations_keyboard.add(
+        *[
+            InlineKeyboardButton(text=item[0], callback_data=item[1])
+            for item in all_stations.items()
+        ]
+    )
+
     bot.edit_message_text(text=answer,
-                          chat_id=call_back.message.chat.id,
+                          chat_id=user.tg_id,
                           message_id=call_back.message.message_id,
                           reply_markup=stations_keyboard)
 
 
 # Choose station callback
-@bot.callback_query_handler(func=lambda call_back:
-                            call_back.message.text == "Выбери домашнюю "
-                                                      "станцию:")
-@bot.callback_query_handler(func=lambda call_back:
-                            call_back.message.text == "Выбери университетскую "
-                                                      "станцию:")
+@bot.callback_query_handler(
+    func=lambda call_back: call_back.message.text == select_home_station
+)
+@bot.callback_query_handler(
+    func=lambda call_back: call_back.message.text == select_univer_station
+)
+@telebot_login.login_required_callback
 def change_home_station_handler(call_back):
+    user = g.current_tbot_user
+
+    is_both_changed = False
     if "домашнюю" in call_back.message.text:
+        if call_back.data == user.univer_station_code:
+            user.univer_station_code = user.home_station_code
+            is_both_changed = True
+        user.home_station_code = call_back.data
         type_station = "Домашняя"
-        is_home = True
     else:
+        if call_back.data == user.home_station_code:
+            user.home_station_code = user.univer_station_code
+            is_both_changed = True
+        user.univer_station_code = call_back.data
         type_station = "Университетская"
-        is_home = False
-    current_station = func.get_station_code(call_back.message.chat.id, is_home)
-    current_another_station = func.get_station_code(call_back.message.chat.id,
-                                                    not is_home)
-    station_title = func.get_key_by_value(all_stations, call_back.data)
-    answer = "{0} станция изменена на <b>{1}</b>\n".format(type_station,
-                                                           station_title)
-    func.change_station(call_back.message.chat.id,
-                        call_back.data, is_home=is_home)
-    if call_back.data == current_another_station:
-        func.change_station(call_back.message.chat.id, current_station,
-                            is_home=not is_home)
+
+    answer = "{0} станция изменена на <b>{1}</b>\n".format(
+        type_station,
+        nf.get_key_by_value(all_stations, call_back.data)
+    )
+    if is_both_changed:
         inline_answer = "{0} Изменены обе станции!".format(emoji["warning"])
         bot.answer_callback_query(callback_query_id=call_back.id,
                                   text=inline_answer, show_alert=True)
+    db.session.commit()
+
     bot.edit_message_text(text=answer,
-                          chat_id=call_back.message.chat.id,
+                          chat_id=user.tg_id,
                           message_id=call_back.message.message_id,
                           parse_mode="HTML")

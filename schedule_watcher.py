9b818b5bc8c379175b10f1635d032b5eb36cdbd1
ivan <st049378@student.spbu.ru>
2018-10-18 23:27:20 +0300

    db models upgraded, sw created

---
+++ schedule_watcher.py
@@ -0,0 +1,22 @@
+from tg_bot import bot
+from app import create_app
+from app.models import Group, Educator
+import requests
+
+
+def watcher():
+    for q in (Educator, Group):
+        for obj in q.query.all():
+            if obj.update_hash(
+                    requests.get("https://timetable.spbu.ru/api/v1/educators/{0}/events".format(obj.id)).content
+            ):
+                for user in obj.members.all():
+                    bot.send_message(
+                        chat_id=user.tg_id,
+                        text="В расписании появились изменения! Заходи и проверь текущую неделю."
+                    )
+
+
+if __name__ == '__main__':
+    with create_app().app_context():
+        watcher()

296c498d8515d76dad3d52c473854a947245fa0e
ivan <st049378@student.spbu.ru>
2018-10-21 18:33:55 +0300

    subscribe checks, working on #90

--- schedule_watcher.py
+++ schedule_watcher.py
@@ -8,10 +8,8 @@ import requests
 def watcher():
     for q in (Educator, Group):
         for obj in q.query.filter(q.id != 0).all():
-            if obj.update_hash(
-                    requests.get("https://timetable.spbu.ru/api/v1/educators/{0}/events".format(obj.id)).content
-            ):
-                for user in obj.current_members:
+            if obj.update_hash(requests.get(obj.get_tt_link(is_api=True)).content):
+                for user in obj.current_members.filter_by(is_subscribed=True):
                     bot.send_message(
                         chat_id=user.tg_id,
                         text=schedule_changed_answer

b57186c782b627b897270ddb5c7e687fb77aefe7
EeOneDown <ione.platonov@yandex.ru>
2022-09-18 21:11:28 +0400

    update tests

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-[![Build Status](https://travis-ci.org/EeOneDown/spbu4u.svg?branch=master)](https://travis-ci.org/EeOneDown/spbu4u)
-[![CodeFactor](https://www.codefactor.io/repository/github/eeonedown/spbu4u/badge)](https://www.codefactor.io/repository/github/eeonedown/spbu4u)
 ![spbu4u](https://user-images.githubusercontent.com/19958147/190895389-4684339f-9b8b-4ab4-ac3e-7e96fc4b6e90.png "Spbu4U")
 
 ## Spbu4U
--- tests/test_flask_app.py
+++ tests/test_flask_app.py
@@ -1,7 +1,32 @@
-import json
 from app.constants import webhook_url_path
-
+from unittest.mock import patch
+from tg_bot import bot
 
 def test_json_data(client):
     response = client.post(f"/tg{webhook_url_path}", json={"update_id": 1})
     assert response.status_code == 200
+
+
+@patch("telebot.TeleBot.process_new_updates", lambda *_, **__: None)
+@patch("app.new_functions.write_log", lambda *_, **__: None)
+def test_message(client):
+    response = client.post(
+        f"/tg{webhook_url_path}", 
+        json={"update_id": 1, "message": {"message_id": 1, "date": 1, "chat": {"id": 1, "type": "private"}}},
+    )
+    assert response.status_code == 200
+
+
+@patch("telebot.TeleBot.process_new_updates", lambda *_, **__: None)
+@patch("app.new_functions.write_log", lambda *_, **__: None)
+def test_callback(client):
+    response = client.post(
+        f"/tg{webhook_url_path}", 
+        json={"update_id": 1, "callback_query": {"id": 1, "from": {"id": 1, "is_bot": False, "first_name": "test"}, "chat_instance": "test"}},
+    )
+    assert response.status_code == 200
+
+
+def test_tg_bot_init():
+    assert bot.message_handlers
+    assert bot.callback_query_handlers
